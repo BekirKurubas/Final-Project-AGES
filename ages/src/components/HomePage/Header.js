0b5972dcf62097ce401f3b0a3dafddd6042af927
Bekir Kurubas <bekirkurubas6767@gmail.com>
2024-03-25 18:14:37 +0100

    HomePage Header has added

--- ages/src/components/HomePage/Header.js
+++ ages/src/components/HomePage/Header.js
@@ -1,7 +1,58 @@
 import React from "react";
+import { FaHome } from "react-icons/fa";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div>
+      <nav className="navbar navbar-expand-lg bg-white justify-content-between">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="/">
+            <FaHome /> AGES
+          </a>
+          <div className="navbar-text">
+            <a
+              className="nav-link active"
+              aria-current="page"
+              href="/language-test"
+            >
+              TELC Language Test
+            </a>
+          </div>
+          <div className="navbar-text">
+            <a
+              className="nav-link active"
+              aria-current="page"
+              href="/model-test"
+            >
+              Let's Try Model Test
+            </a>
+          </div>
+        </div>
+      </nav>
+      <nav
+        className="navbar navbar-expand-lg"
+        style={{
+          backgroundColor: "#FF0000",
+          maxWidth: "100%",
+          height: "auto",
+          padding: "0",
+        }}
+      >
+        <div className="container-fluid">
+          <div className="navbar-text mx-auto">
+            <a
+              className="nav-link active"
+              aria-current="page"
+              href="/language-test"
+              style={{ color: "#FFFFFF" }}
+            >
+              <h1>AGES</h1>
+            </a>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
 };
 
 export default Header;
