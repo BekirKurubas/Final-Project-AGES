ce2f57f224f26c782f6d74ed311554bf6ba4748b
Bekir Kurubas <bekirkurubas6767@gmail.com>
2024-03-25 18:54:25 +0100

    user opinions and photos have added

--- ages/src/components/HomePage/Content.js
+++ ages/src/components/HomePage/Content.js
@@ -1,6 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const Content = () => {
+  const [people, setPeople] = useState([]);
+
+  useEffect(() => {
+    fetch("https://randomuser.me/api/?results=3")
+      .then((response) => response.json())
+      .then((data) => setPeople(data.results))
+      .catch((error) => console.error("Error fetching data: ", error));
+  }, []);
+
+  const Card = ({ title, text, imgSrc, name }) => {
+    return (
+      <div className="col-md-4 mb-4">
+        <div className="card">
+          <img src={imgSrc} className="card-img-top" alt="Person" />
+          <div className="card-body">
+            <h5 className="card-title">{title}</h5>
+            <p className="card-text">{text}</p>
+            <p className="card-text">{name}</p>
+          </div>
+        </div>
+      </div>
+    );
+  };
+
   return (
     <div>
       <div
@@ -168,6 +192,23 @@ const Content = () => {
               </p>
             </div>
           </div>
+        </div>
+      </div>
+      <div className="text-center" style={{ marginBottom: "60px" }}>
+        <h1>Our Clients Speak</h1>
+        <p>We have been working with clients around the world</p>
+      </div>
+      <div className="container-fluid">
+        <div className="row">
+          {people.map((person, index) => (
+            <Card
+              key={index}
+              title={`Title ${index + 1}`}
+              text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla felis nisi, molestie non metus sit amet, consectetur aliquet lorem."
+              imgSrc={person.picture.large}
+              name={`${person.name.first} ${person.name.last}`}
+            />
+          ))}
         </div>
       </div>
     </div>
