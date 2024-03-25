95bcc0a224bcbdb1036eb9adbb997070fbe54418
Bekir Kurubas <bekirkurubas6767@gmail.com>
2024-03-25 15:46:58 +0100

    HomePage template has created

---
+++ ages/src/Pages/HomePage.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "../components/HomePage/Header";
+import Content from "../components/HomePage/Content";
+import Footer from "../components/HomePage/Footer";
+
+const HomePage = () => {
+  return (
+    <div>
+      <Header />
+      <Content />
+      <Footer />
+    </div>
+  );
+};
+
+export default HomePage;
