415147f7b79d9fb8f33f3aaf3d6699274028582f
Bekir Kurubas <bekirkurubas6767@gmail.com>
2024-03-26 15:32:49 +0100

    two pictures have added to HomePage

--- ages/src/components/HomePage/Content.js
+++ ages/src/components/HomePage/Content.js
@@ -291,7 +291,34 @@ const Content = () => {
         </div>
       </div>
 
-      <div className="text-center" style={{ marginBottom: "100px" }}>
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-md-6 mb-5">
+            <div className="row">
+              <div style={{ padding: "20px" }}>
+                <img
+                  src="https://github.com/BekirKurubas/Final-Project-Photos/raw/main/Project%20Photos/telc-B1-exam-book.jpg"
+                  alt="Summary Writing"
+                  style={{ width: "100%", height: "auto" }}
+                />
+              </div>
+            </div>
+          </div>
+          <div className="col-md-6 mb-4">
+            <div className="row">
+              <div style={{ padding: "20px" }}>
+                <img
+                  src="https://github.com/BekirKurubas/Final-Project-Photos/raw/main/Project%20Photos/Pr%C3%BCfung-Ablauf.png"
+                  alt="Summary Writing"
+                  style={{ width: "100%", height: "auto" }}
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="text-center" style={{ marginBottom: "80px" }}>
         <h1>Our Clients Speak</h1>
         <p>We have been working with clients around the world</p>
       </div>
