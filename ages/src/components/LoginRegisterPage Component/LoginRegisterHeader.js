1863bbee33fb46b96042f33835393d70fb1bc5fa
Bekir Kurubas <bekirkurubas6767@gmail.com>
2024-03-27 09:43:55 +0100

    LoginRegisterPage Header has made responsive

--- ages/src/components/LoginRegisterPage Component/LoginRegisterHeader.js
+++ ages/src/components/LoginRegisterPage Component/LoginRegisterHeader.js
@@ -5,25 +5,45 @@ import { Link } from "react-router-dom";
 const LoginRegisterHeader = () => {
   return (
     <div>
-      <nav className="navbar navbar-expand-lg bg-white justify-content-between">
+      <nav className="navbar navbar-expand-md bg-white justify-content-between">
         <div className="container-fluid">
           <a className="navbar-brand" href="/">
             <FaHome /> AGES
           </a>
-          <div className="navbar-text">
-            <a className="nav-link active" aria-current="page" href="/Telc">
-              What is TELC Language Test ?
-            </a>
-          </div>
-          <div className="text-center" style={{ marginBottom: "20px" }}>
-            <Link to="/login-register" className="btn btn-primary">
-              Let's try Model Test
-            </Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav mx-auto">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="/Telc">
+                  What is TELC Language Test ?
+                </a>
+              </li>
+            </ul>
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <Link
+                  to="/login-register"
+                  className="nav-link btn btn-primary btn-lg"
+                >
+                  Let's try Model Test
+                </Link>
+              </li>
+            </ul>
           </div>
         </div>
       </nav>
       <nav
-        className="navbar navbar-expand-lg"
+        className="navbar navbar-expand-md"
         style={{
           backgroundColor: "#FF0000",
           maxWidth: "100%",
