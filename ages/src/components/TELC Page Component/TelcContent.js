d1e375ed8755996aeba956a02a5aa586b707c0d4
Bekir Kurubas <bekirkurubas6767@gmail.com>
2024-03-30 21:27:05 +0100

    WhyTelcRow component has created

--- ages/src/components/TELC Page Component/TelcContent.js
+++ ages/src/components/TELC Page Component/TelcContent.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ExamRow from "./ExamRow";
 import LevelB1Row from "./LevelB1Row";
+import WhyTelcRow from "./WhyTelcRow";
 
 const TelcContent = () => {
   return (
@@ -17,58 +18,7 @@ const TelcContent = () => {
       </div>
       <div className="container-fluid">
         <div className="row">
-          <div className="col-md-6 mb-5">
-            <div
-              style={{
-                padding: "10px",
-                fontSize: "12px",
-                whiteSpace: "pre-line",
-                marginTop: "20px",
-              }}
-            >
-              <h1
-                style={{
-                  fontSize: "30px",
-                }}
-              >
-                Why to be choosed <br /> a Telc language test?
-              </h1>
-              <br />
-              <p>
-                Telc language tests are recognised worldwide by schools,
-                universities, employers and government authorities as valid
-                proof of language proficiency at all six levels of the Common
-                European Framework of Reference for Languages (CEFR). As a full
-                member of ALTE (Association of Language Testers in Europe), telc
-                is committed to complying with international quality standards.
-                Regular audits are carried out to ensure we meet these
-                standards.
-                <br />
-                <br />
-                To learn more about TELC German Exams, please click{" "}
-                <span>
-                  <a
-                    href="https://www.telc.net/"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    here
-                  </a>
-                </span>
-              </p>
-            </div>
-          </div>
-          <div className="col-md-6 mb-5">
-            <div className="row">
-              <div style={{ padding: "20px" }}>
-                <img
-                  src="https://github.com/BekirKurubas/Final-Project-Photos/raw/main/Project%20Photos/smile.jpg"
-                  alt="smile"
-                  style={{ width: "100%", height: "280px" }}
-                />
-              </div>
-            </div>
-          </div>
+          <WhyTelcRow />
         </div>
       </div>
     </>
--- ages/src/components/TELC Page Component/WhyTelcRow.js
+++ ages/src/components/TELC Page Component/WhyTelcRow.js
@@ -1,31 +1,58 @@
 const WhyTelcRow = () => {
   return (
-    <div className="container-fluid">
-      <div className="row">
-        <div className="col-md-6 mb-5">
-          <div className="row">
-            <div style={{ padding: "20px" }}>
-              <img
-                src="https://github.com/BekirKurubas/Final-Project-Photos/raw/main/Project%20Photos/telc-B1-exam-book.jpg"
-                alt="telc-B1-exam-book"
-                style={{ width: "100%", height: "300px" }}
-              />
-            </div>
-          </div>
+    <>
+      <div className="col-md-6 mb-5">
+        <div
+          style={{
+            padding: "10px",
+            fontSize: "12px",
+            whiteSpace: "pre-line",
+            marginTop: "20px",
+          }}
+        >
+          <h1
+            style={{
+              fontSize: "30px",
+            }}
+          >
+            Why to be choosed <br /> a Telc language test?
+          </h1>
+          <br />
+          <p>
+            Telc language tests are recognised worldwide by schools,
+            universities, employers and government authorities as valid proof of
+            language proficiency at all six levels of the Common European
+            Framework of Reference for Languages (CEFR). As a full member of
+            ALTE (Association of Language Testers in Europe), telc is committed
+            to complying with international quality standards. Regular audits
+            are carried out to ensure we meet these standards.
+            <br />
+            <br />
+            To learn more about TELC German Exams, please click{" "}
+            <span>
+              <a
+                href="https://www.telc.net/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                here
+              </a>
+            </span>
+          </p>
         </div>
-        <div className="col-md-6 mb-5">
-          <div className="row">
-            <div style={{ padding: "20px" }}>
-              <img
-                src="https://github.com/BekirKurubas/Final-Project-Photos/raw/main/Project%20Photos/Pr%C3%BCfung-Ablauf.png"
-                alt="Prüfung-Ablauf"
-                style={{ width: "100%", height: "auto" }}
-              />
-            </div>
+      </div>
+      <div className="col-md-6 mb-5">
+        <div className="row">
+          <div style={{ padding: "20px" }}>
+            <img
+              src="https://github.com/BekirKurubas/Final-Project-Photos/raw/main/Project%20Photos/smile.jpg"
+              alt="smile"
+              style={{ width: "100%", height: "280px" }}
+            />
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 export default WhyTelcRow;
