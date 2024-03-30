ebbb375c36d7df26a148a2c751df0b6654b30953
Bekir Kurubas <bekirkurubas6767@gmail.com>
2024-03-30 17:43:18 +0100

    Aufbau components has created

--- ages/src/components/HomePage Component/HomePageContent.js
+++ ages/src/components/HomePage Component/HomePageContent.js
@@ -5,6 +5,7 @@ import GoalsRow from "./GoalsRow";
 import KeyFeaturesRow from "./KeyFeatures";
 import ConclusionRow from "./ConclusionRow";
 import IconsCardRow from "./IconsCardRow";
+import AufbauRow from "./AufbauRow";
 
 const Content = () => {
   const [people, setPeople] = useState([]);
@@ -45,32 +46,7 @@ const Content = () => {
 
       <IconsCardRow />
 
-      <div className="container-fluid">
-        <div className="row">
-          <div className="col-md-6 mb-5">
-            <div className="row">
-              <div style={{ padding: "20px" }}>
-                <img
-                  src="https://github.com/BekirKurubas/Final-Project-Photos/raw/main/Project%20Photos/telc-B1-exam-book.jpg"
-                  alt="telc-B1-exam-book"
-                  style={{ width: "100%", height: "300px" }}
-                />
-              </div>
-            </div>
-          </div>
-          <div className="col-md-6 mb-5">
-            <div className="row">
-              <div style={{ padding: "20px" }}>
-                <img
-                  src="https://github.com/BekirKurubas/Final-Project-Photos/raw/main/Project%20Photos/Pr%C3%BCfung-Ablauf.png"
-                  alt="Prüfung-Ablauf"
-                  style={{ width: "100%", height: "auto" }}
-                />
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+      <AufbauRow />
 
       <div className="text-center" style={{ marginBottom: "80px" }}>
         <h1>Our Clients Speak</h1>
