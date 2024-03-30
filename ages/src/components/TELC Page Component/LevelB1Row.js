71b6e8e8eaf2adf82d35acbf1a7f55254abe3b44
Bekir Kurubas <bekirkurubas6767@gmail.com>
2024-03-30 21:23:20 +0100

    LevelB1Row component has created

--- ages/src/components/TELC Page Component/LevelB1Row.js
+++ ages/src/components/TELC Page Component/LevelB1Row.js
@@ -1,31 +1,37 @@
-const AufbauRow = () => {
+const LevelB1Row = () => {
   return (
-    <div className="container-fluid">
-      <div className="row">
-        <div className="col-md-6 mb-5">
-          <div className="row">
-            <div style={{ padding: "20px" }}>
-              <img
-                src="https://github.com/BekirKurubas/Final-Project-Photos/raw/main/Project%20Photos/telc-B1-exam-book.jpg"
-                alt="telc-B1-exam-book"
-                style={{ width: "100%", height: "300px" }}
-              />
-            </div>
-          </div>
-        </div>
-        <div className="col-md-6 mb-5">
-          <div className="row">
-            <div style={{ padding: "20px" }}>
-              <img
-                src="https://github.com/BekirKurubas/Final-Project-Photos/raw/main/Project%20Photos/Pr%C3%BCfung-Ablauf.png"
-                alt="Prüfung-Ablauf"
-                style={{ width: "100%", height: "auto" }}
-              />
-            </div>
-          </div>
+    <>
+      <div className="col-md-6 mb-5">
+        <div style={{ padding: "20px" }}>
+          <img
+            src="https://github.com/BekirKurubas/Final-Project-Photos/raw/main/Project%20Photos/three-friend.jpg"
+            alt="Three-friend"
+            style={{ width: "100%", height: "auto" }}
+          />
         </div>
       </div>
-    </div>
+      <div className="col-md-6 mb-5">
+        <div
+          style={{
+            padding: "10px",
+            fontSize: "13px",
+            whiteSpace: "pre-line",
+          }}
+        >
+          <h1>The CEFR level B1</h1>
+          <br />
+          <p>
+            Learners at B1 level can communicate in a simple and coherent way in
+            daily life, at work, and when travelling write or speak about a
+            range of topics in their own area of interest. participate in
+            conversations about family, hobbies, work, and current events,
+            reporting experiences, describing goals, and giving reasons for
+            opinions. generally use grammar and language correctly. understand
+            and produce simple, connected texts on familiar topics.
+          </p>
+        </div>
+      </div>
+    </>
   );
 };
-export default AufbauRow;
+export default LevelB1Row;
--- ages/src/components/TELC Page Component/TelcContent.js
+++ ages/src/components/TELC Page Component/TelcContent.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ExamRow from "./ExamRow";
+import LevelB1Row from "./LevelB1Row";
 
 const TelcContent = () => {
   return (
@@ -11,37 +12,7 @@ const TelcContent = () => {
       </div>
       <div className="container-fluid">
         <div className="row">
-          <div className="col-md-6 mb-5">
-            <div style={{ padding: "20px" }}>
-              <img
-                src="https://github.com/BekirKurubas/Final-Project-Photos/raw/main/Project%20Photos/three-friend.jpg"
-                alt="Three-friend"
-                style={{ width: "100%", height: "auto" }}
-              />
-            </div>
-          </div>
-          <div className="col-md-6 mb-5">
-            <div
-              style={{
-                padding: "10px",
-                fontSize: "13px",
-                whiteSpace: "pre-line",
-              }}
-            >
-              <h1>The CEFR level B1</h1>
-              <br />
-              <p>
-                Learners at B1 level can communicate in a simple and coherent
-                way in daily life, at work, and when travelling write or speak
-                about a range of topics in their own area of interest.
-                participate in conversations about family, hobbies, work, and
-                current events, reporting experiences, describing goals, and
-                giving reasons for opinions. generally use grammar and language
-                correctly. understand and produce simple, connected texts on
-                familiar topics.
-              </p>
-            </div>
-          </div>
+          <LevelB1Row />
         </div>
       </div>
       <div className="container-fluid">
