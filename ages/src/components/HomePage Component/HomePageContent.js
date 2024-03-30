f8c1563d085d33b56c4508c2dd608fd1cfdb8f01
Bekir Kurubas <bekirkurubas6767@gmail.com>
2024-03-30 17:34:48 +0100

    Conclusion components has created

--- ages/src/components/HomePage Component/HomePageContent.js
+++ ages/src/components/HomePage Component/HomePageContent.js
@@ -7,6 +7,7 @@ import ExecutiveSummaryRow from "./ExecutiveSummaryRow";
 import HomePagePictureRow from "./HomePagesPictureRow";
 import GoalsRow from "./GoalsRow";
 import KeyFeaturesRow from "./KeyFeatures";
+import ConclusionRow from "./ConclusionRow";
 
 const Content = () => {
   const [people, setPeople] = useState([]);
@@ -54,40 +55,7 @@ const Content = () => {
 
       <KeyFeaturesRow />
 
-      <div className="container-fluid">
-        <div className="row">
-          <div className="col-md-6 mb-4">
-            <div style={{ padding: "20px" }}>
-              <img
-                src="https://github.com/BekirKurubas/Final-Project-Photos/raw/main/Project%20Photos/conclusion.jpg"
-                alt="Conculusion"
-                style={{ marginBottom: "40px", width: "100%", height: "auto" }}
-              />
-            </div>
-          </div>
-          <div className="col-md-6 mb-4">
-            <div
-              style={{
-                padding: "10px",
-                fontSize: "23x",
-                whiteSpace: "pre-line",
-                marginBottom: "120px",
-              }}
-            >
-              <h1>Conclusion</h1>
-              <p>
-                In conclusion, the Amazing German Exam Simulation (AGES) project
-                aims to revolutionize B1 Telc GermanExam preparation by
-                providing an engaging, realistic, and efficient simulation of
-                the exam. With a focus on user experience and comprehensive
-                practice, AGES endeavors to be the go-to platform for
-                individuals aiming for success in their language proficiency
-                journey.
-              </p>
-            </div>
-          </div>
-        </div>
-      </div>
+      <ConclusionRow />
 
       <div className="container-fluid">
         <div className="row text-center">
