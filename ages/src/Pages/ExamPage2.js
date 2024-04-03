5a544c745406b572af8733e3803f6bd743bc4472
Bekir Kurubas <bekirkurubas6767@gmail.com>
2024-04-03 18:58:24 +0200

    ExamPage2 has fixed

--- ages/src/Pages/ExamPage2.js
+++ ages/src/Pages/ExamPage2.js
@@ -3,25 +3,11 @@ import ExamPage2Header from "../components/Exam Page 2/ExamPage2Header";
 import ExamPage2Content from "../components/Exam Page 2/ExamPage2Content";
 import Footer from "../components/ConstantComponent/Footer";
 
-const ExamPage2 = () => {
-  const startTimer = (timeInSeconds) => {
-    // Sınav zamanlayıcısını başlatmak için burada gerekli kodu ekleyin
-  };
-
-  const handleStartExam = () => {
-    // Sınavın süresini başlat
-    startTimer(90 * 60);
-  };
-
-  const lv2Urls = [
-    "https://github.com/BekirKurubas/Final-Project-Photos/raw/main/Project%20Photos/Lesen-Verstehen-2.1_page-0001.jpg",
-    "https://github.com/BekirKurubas/Final-Project-Photos/raw/main/Project%20Photos/Lesen-Verstehen-2.2_page-0001.jpg",
-  ];
-
+const ExamPage2 = ({ lv2Urls }) => {
   return (
     <div>
       <ExamPage2Header />
-      <ExamPage2Content lv2Urls={lv2Urls} startTimer={handleStartExam} />
+      <ExamPage2Content lv2Urls={lv2Urls} />
       <Footer />
     </div>
   );
