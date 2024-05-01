240dcc209e7ae98d65ef62994d16021d05a1f573
Bekir Kurubas <bekirkurubas6767@gmail.com>
2024-05-01 16:20:10 +0200

    extra footer has deleted from LoginPage

--- ages/src/Pages/LoginPage.js
+++ ages/src/Pages/LoginPage.js
@@ -1,14 +1,12 @@
 import React from "react";
 import LoginPageHeader from "../components/LoginPage Component/LoginPageHeader";
 import LoginPageContent from "../components/LoginPage Component/LoginPageContent";
-import Footer from "../components/ConstantComponent/Footer";
 
 const LoginPage = () => {
   return (
     <div>
       <LoginPageHeader />
       <LoginPageContent />
-      <Footer />
     </div>
   );
 };
