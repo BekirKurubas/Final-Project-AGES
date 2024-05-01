64bdbb535be25d4bcb3137cb3a87b92ed43de816
Bekir Kurubas <bekirkurubas6767@gmail.com>
2024-05-01 16:21:57 +0200

    extra footer has deleted from TelcPage

--- ages/src/Pages/TelcPage.js
+++ ages/src/Pages/TelcPage.js
@@ -1,14 +1,12 @@
 import React from "react";
 import Telcheader from "../components/TELC Page Component/TelcHeader";
 import TelcContent from "../components/TELC Page Component/TelcContent";
-import Footer from "../components/ConstantComponent/Footer";
 
 const TelcPage = () => {
   return (
     <div>
       <Telcheader />
       <TelcContent />
-      <Footer />
     </div>
   );
 };
