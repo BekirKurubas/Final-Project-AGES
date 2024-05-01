5bd45c12fe406320d2fc460c68a7f952dadba75b
Bekir Kurubas <bekirkurubas6767@gmail.com>
2024-05-01 16:15:22 +0200

    extra footer has deleted from HomePage

--- ages/src/Pages/HomePage.js
+++ ages/src/Pages/HomePage.js
@@ -1,14 +1,12 @@
 import React from "react";
 import HomePageHeader from "../components/HomePage Component/HomePageHeader";
 import Content from "../components/HomePage Component/HomePageContent";
-import Footer from "../components/ConstantComponent/Footer";
 
 const HomePage = () => {
   return (
     <div>
       <HomePageHeader />
       <Content />
-      <Footer />
     </div>
   );
 };
