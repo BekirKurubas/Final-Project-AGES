1c55ef7fc0d372bafc6bf4393215ca3835330301
Bekir Kurubas <bekirkurubas6767@gmail.com>
2024-06-01 11:48:33 +0200

    Netlify settings has added

--- ages/src/components/home-page/HomePageContent.js
+++ ages/src/components/home-page/HomePageContent.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ExecutiveSummaryRow from "./ExecutiveSummaryRow";
 import HomePagePictureRow from "./HomePagesPictureRow";
 import GoalsRow from "./GoalsRow";
@@ -9,8 +9,39 @@ import AufbauRow from "./AufbauRow";
 import ClientRow from "./ClientRow";
 import PersonCardsRow from "./PersonCardsRow";
 
-
 const Content = () => {
+  const [exams, setExams] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const backendUrl = process.env.REACT_APP_API_ENDPOINT;
+
+    fetch(`${backendUrl}/exam`)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then(data => {
+        setExams(data);
+        setLoading(false);
+      })
+      .catch(error => {
+        console.error('Error:', error);
+        setError(error);
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
 
   return (
     <div>
@@ -31,6 +62,14 @@ const Content = () => {
       <ClientRow />
 
       <PersonCardsRow />
+      <div>
+        <h1>Exam List</h1>
+        <ul>
+          {exams.map(exam => (
+            <li key={exam.id}>{exam.name}</li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
